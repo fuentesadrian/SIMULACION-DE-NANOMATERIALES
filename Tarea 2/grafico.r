6ae46a073a27718b3c1bf0456c89a68af3445872
Adrian Garcia <71805592+fuentesadrian@users.noreply.github.com>
2021-03-03 08:28:00 -0600

    Update grafico.r

--- Tarea 2/grafico.r
+++ Tarea 2/grafico.r
@@ -1,4 +1,4 @@
 paso= 1:30
 vivos= c(32,24,23,21,13,16,8, 7, 8,9, 6 ,4 ,7,10,10,9,8,9,10,9,9,7,7,4,2,5,3,3,2,1)
 
-plot(paso, vivos,type="l", main="grafico vivos por año", xlab="Años trascurridos", ylab="vivos", pch=16,)
+plot(paso, vivos, main="Grafico de vivos", xlab="Años trascurridos", ylab="vivos",pch=16, )
